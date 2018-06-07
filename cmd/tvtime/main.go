075463391dbab1e33661fe07062c06bdd7d5e532
Patrik Cyvoct <patrik@ptrk.io>
2018-06-07 15:41:10 +0200

    Add cmd
    Signed-off-by: Patrik Cyvoct <patrik@ptrk.io>

---
+++ cmd/tvtime/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tvtime "github.com/Sh4d1/go-tvtime"
+	"github.com/urfave/cli"
+)
+
+var version = "master"
+
+func main() {
+	app := cli.NewApp()
+
+	app.Name = "tvtime"
+	app.Version = version
+	app.Author = "Patrik Cyvoct (patrik@ptrk.io)"
+	app.Usage = "tvtime"
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "login",
+			Usage: "Login to TV Time",
+			Action: func(c *cli.Context) error {
+				return tvtime.Login()
+			},
+		},
+		{
+			Name:  "user",
+			Usage: "Display current user",
+			Action: func(c *cli.Context) error {
+				return tvtime.DisplayUser()
+			},
+		},
+		{
+			Name:  "upcoming",
+			Usage: "Display upcoming episodes",
+			Action: func(c *cli.Context) error {
+				return tvtime.DisplayUpcoming()
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+}
